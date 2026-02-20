3b17088bddc9723d7ddbd4f923e2824eb477dc1b
AjAjish <abiajish2202@email.com>
2026-02-20 14:28:02 +0530

    Adding postgresql

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -82,18 +82,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database - MySQL Configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME', default='manufacturing_erp'),
         'USER': config('DB_USER', default='root'),
         'PASSWORD': config('DB_PASSWORD', default=''),
         'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='3306'),
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
+
+# OPTIONS =  {
+#     'charset': 'utf8mb4',
+#     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+# }
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
